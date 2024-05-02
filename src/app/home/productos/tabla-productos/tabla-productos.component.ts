84efcfba0c68a10fa3f663ff172b393f76987149
djarae <djaraestivill@gmail.com>
2024-05-01 23:34:20 -0400

    Funciona Binding y Llamada Api
    Next step , CREATE

--- src/app/home/productos/tabla-productos/tabla-productos.component.ts
+++ src/app/home/productos/tabla-productos/tabla-productos.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'app-tabla-productos',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./tabla-productos.component.css']
 })
 export class TablaProductosComponent {
+  arr=[{id:"0",nombre:"section1",stock:"0"}];
+
+  async  ngOnInit(): Promise<void> {
+
+    let hookFiltroNProducto:string="Kirby";
+    let hookFiltroSProducto:number=20;
+    localStorage.setItem("paginacion","1");
+  const response = await axios.get(
+    "http://127.0.0.1:8000/getListadoProductos?nombreProd='"+hookFiltroNProducto+"'&stockProd='"+hookFiltroSProducto+"'"+"&paginacion='"+localStorage.getItem("paginacion")+"'",{withCredentials: true }
+  
+  )
+
+  let ListadoProductosFiltrado: (string | number)[] =[];
+
+    for (let i=0;(i<response.data.datax.length);i++){
+          ListadoProductosFiltrado.push(response.data.datax[i]);
+          this.arr.push(response.data.datax[i]);
+    }
+    console.log(ListadoProductosFiltrado)
+  
+ 
+   
+  }
+
 
 }
